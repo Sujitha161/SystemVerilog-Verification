08d2bfe6d3bcf886fa68f74b3545e8db70b1265e
Sujitha_Sivakumar <sujithasiva161@gmail.com>
2026-05-20 09:53:51 +0530

    Create 8.Design.sv

---
+++ D - Flip Flop/8.Design.sv
@@ -0,0 +1,14 @@
+
+module d_ff(
+  input clk,rst,
+  input d,
+  output reg q);
+   
+  always@(posedge clk or negedge rst)begin
+    if(!rst)
+      q<=0;
+    else
+      q<=d;
+  end
+endmodule
+    
